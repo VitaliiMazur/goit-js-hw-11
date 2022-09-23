df01a7559acb20e4df1b41c0edb0065e73e64013
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-09-23 11:06:55 +0300

    початок2

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { FetchImages } from './fetchImages';
+import { refs } from './refs';
+
+const fetchImages = new FetchImages();
+
+removeButtonLoadMore();
